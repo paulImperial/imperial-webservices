abd02eab00fa1b3dff2247d1f309573493a3a134
Paul Mason <paul.mason@experian.com>
2019-10-05 18:17:37 +0100

    update

--- function/index.js
+++ function/index.js
@@ -1,18 +1,6 @@
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
-const app = express();
-const bodyParser = require('body-parser');
-
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-
-module.exports = app;
-module.exports.handler = serverless(app);
+exports.handler = (event, context, callback) => {
+	callback(null, {
+	  statusCode: 200,
+	  body: 'Hello, world!',
+	});
+  };
